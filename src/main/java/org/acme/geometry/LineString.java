075369ce3709414a2f4fa007f545309a34820954
MMge <mailys.monge@laposte.net>
2022-01-03 14:22:17 +0100

    finish implements question 2

--- src/main/java/org/acme/geometry/LineString.java
+++ src/main/java/org/acme/geometry/LineString.java
@@ -1,6 +1,29 @@
 package org.acme.geometry;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LineString implements Geometry{
+    
+    private List<Point> points;
+
+    LineString(){
+        this.points = new ArrayList<Point>();
+    }
+
+    LineString(List<Point> points){
+        this.points = points;
+    }
+
+    public int getNumPoints(){
+        return points.size();
+    }
+
+    public Point getPointN(int i){
+        return points.get(i);
+    }
+
     public String getType(){
         return "LineString";
     }
--- src/main/java/org/acme/geometry/Point.java
+++ src/main/java/org/acme/geometry/Point.java
@@ -3,6 +3,15 @@ package org.acme.geometry;
 public class Point implements Geometry {
 
     private Coordinate coordinate;
+
+    Point(){
+        this.coordinate = new Coordinate();
+    }
+
+    Point(Coordinate coordinate){
+        this.coordinate = coordinate;
+    }
+
     public String getType(){
         return "Point";
     }
