56df40d0a40d9c3f097ff4cef7b6e1d63b0cbce4
MMge <mailys.monge@laposte.net>
2022-01-03 15:52:39 +0100

    update test for points and linestring with isEmpty()

--- src/test/java/org/acme/geometry/LineStringTest.java
+++ src/test/java/org/acme/geometry/LineStringTest.java
@@ -8,6 +8,14 @@ import org.junit.Test;
 
 public class LineStringTest {
     public static final double EPSILON = 1.0e-15;
+	//Init Coordinbate and List<Points> for testing
+	public static final Coordinate c1 = new Coordinate(1.0,5.0);
+	public static final Coordinate c2 = new Coordinate(3.0,6.0);
+
+	public static final Point p1 = new Point(c1);
+	public static final Point p2 = new Point(c2);
+	public List<Point> points = new ArrayList<Point>();
+
 
 	@Test
 	public void testDefaultConstructor(){
@@ -17,13 +25,9 @@ public class LineStringTest {
 
 	@Test
 	public void testConstructor(){
-        Coordinate c1 = new Coordinate(1.0,5.0);
-		Point p1 = new Point(c1);
-        Coordinate c2 = new Coordinate(3.0,6.0);
-		Point p2 = new Point(c2);
-        List<Point> points = new ArrayList<Point>();
-        points.add(p1);
-        points.add(p2);
+        
+		points.add(p1);
+		points.add(p2);
         LineString line = new LineString(points);
 
 		Assert.assertEquals(2,line.getNumPoints(), EPSILON);
@@ -38,5 +42,15 @@ public class LineStringTest {
 		LineString line = new LineString();
 		Assert.assertEquals("LineString",line.getType());
 
+	}
+
+	@Test
+	public void testIsEmpty(){
+		points.add(p1);
+		points.add(p2);
+		LineString line1 = new LineString();
+		LineString line2 = new LineString(points);
+		Assert.assertEquals(true,line1.isEmpty());
+		Assert.assertEquals(false,line2.isEmpty());
 	}
 }
--- src/test/java/org/acme/geometry/PointTest.java
+++ src/test/java/org/acme/geometry/PointTest.java
@@ -6,16 +6,19 @@ import org.junit.Test;
 public class PointTest {
     public static final double EPSILON = 1.0e-15;
 
+	//Init Coordinate
+
+	public static final Coordinate c = new Coordinate(1.0,5.0);
+
 	@Test
 	public void testDefaultConstructor(){
 		Point p = new Point();
-		Assert.assertEquals(0.0, p.getCoordinate().getX(), EPSILON);
-		Assert.assertEquals(0.0, p.getCoordinate().getY(), EPSILON);
+		Assert.assertEquals(Double.NaN, p.getCoordinate().getX(), EPSILON);
+		Assert.assertEquals(Double.NaN, p.getCoordinate().getY(), EPSILON);
 	}
 
 	@Test
 	public void testConstructor(){
-        Coordinate c = new Coordinate(1.0,5.0);
 		Point p = new Point(c);
 		Assert.assertEquals(1.0, p.getCoordinate().getX(), EPSILON);
 		Assert.assertEquals(5.0, p.getCoordinate().getY(), EPSILON);
@@ -25,6 +28,15 @@ public class PointTest {
 	public void testGetType(){
 		Point p = new Point();
 		Assert.assertEquals("Point",p.getType());
+
+	}
+
+	@Test
+	public void isEmpty(){
+		Point p1 = new Point();
+		Point p2 = new Point(c);
+		Assert.assertEquals(true,p1.isEmpty());
+		Assert.assertEquals(false,p2.isEmpty());
 
 	}
 }
