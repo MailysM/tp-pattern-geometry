a3db27635391523869223882b6a3a54aa5329371
MMge <mailys.monge@laposte.net>
2022-01-03 13:55:02 +0100

    feat : implements coordinate class

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -1,5 +1,23 @@
 package org.acme.geometry;
 
 public class Coordinate {
+    private double x;
+    private double y;
 
+    Coordinate(){
+
+    }
+
+    Coordinate(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    double getX(){
+        return x;
+    }
+
+    double getY(){
+        return y;
+    }
 }
