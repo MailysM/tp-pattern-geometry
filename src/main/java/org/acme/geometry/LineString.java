c684c0abc3954ba5ad6d3c36ae485fb448703d81
MMge <mailys.monge@laposte.net>
2022-01-03 14:13:13 +0100

    add Point and LineString Class

---
+++ src/main/java/org/acme/geometry/LineString.java
@@ -0,0 +1,7 @@
+package org.acme.geometry;
+
+public class LineString implements Geometry{
+    public String getType(){
+        return "LineString";
+    }
+}
---
+++ src/main/java/org/acme/geometry/Point.java
@@ -0,0 +1,10 @@
+package org.acme.geometry;
+
+public class Point implements Geometry {
+
+    private Coordinate coordinate;
+    public String getType(){
+        return "Point";
+    }
+    
+}
